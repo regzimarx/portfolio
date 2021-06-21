e6add7373b1ece57601cd862a7ba408f849de86a
regzimarx <gergimarjohnalinsangao@gmail.com>
2021-06-21 20:35:16 +0800

    Add styles and info to about page
    Add animate css. Improve layouts and styles of about page.
    Add contact info. Add bio. Add tech stacks.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,9 +9,9 @@ function Home() {
       <div className="container">
         <div className="hero row">
           <div className="col-md-12 col-lg-12 hero-text">
-            <h1>Regzimarx Alinsangao</h1>
-            <h2>Hi! I'm a web developer specializing in Django Web Framework and ReactJS</h2>
-            <div className="social-links">
+            <h1 className="animate__animated animate__backInUp">Regzimarx Alinsangao.</h1>
+            <h2 className="animate__animated animate__backInUp animate__delay-1s">Hi! I'm a web developer specializing in Django Web Framework and ReactJS</h2>
+            <div className="social-links animate__animated animate__lightSpeedInRight animate__delay-2s">
               <a href="#">
                 <i className="bi bi-facebook"></i>
               </a>
